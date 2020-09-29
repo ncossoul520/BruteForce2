00c0f94f6f326dbe98c9e1d16e34fe7a474b5045
Nolann <ncossoul@gmail.com>
2020-09-28 23:03:58 -0700

    Exercise 0-2 done

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,9 +1,64 @@
+import java.util.Arrays;
+
 public class Exercise1 {
+
     public static void RunA() {
 
+        int[] list_nums = new int[] {3, 9, 5, -1, -3};
+        int target = 7;
+        boolean possible = canMakeSum( list_nums, target );  // returns true because 3 + 5 + (-1) = 7
+        System.out.println( possible + " " + Arrays.toString(list_nums) + " = " + target );
+
+        target = 6;
+        possible = canMakeSum( list_nums, target );  // returns false because you can’t make 6 by adding 3 elements from the list
+        System.out.println( possible + " " + Arrays.toString(list_nums) + " = " + target );
+
+        target = 27;
+        possible = canMakeSum( list_nums, target );  // returns false because 9 + 9 + 9 = 27
+        System.out.println( possible + " " + Arrays.toString(list_nums) + " = " + target );
+    }
+
+    private static boolean canMakeSum(int[] list_nums, int target) {
+
+        for (int i = 0; i < list_nums.length; i++) {
+            for (int j = i + 1; j < list_nums.length; j++) {
+                for (int k = j + 1; k < list_nums.length; k++) {
+                    if (list_nums[i] + list_nums[j] + list_nums[k] == target) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     public static void RunB() {
 
+        int[] list_nums = new int[] {3, 9, 5, -1, -3};
+        int target = 7;
+        boolean possible = canMakeSum2( list_nums, target );  // returns true because 3 + 5 + (-1) = 7
+        System.out.println( possible + " " + Arrays.toString(list_nums) + " = " + target );
+
+        target = 13;
+        possible = canMakeSum2( list_nums, target );  // returns true because 5 + 5 + 3 = 13
+        System.out.println( possible + " " + Arrays.toString(list_nums) + " = " + target );
+
+        target = -9;
+        possible = canMakeSum2( list_nums, target );  // returns true because -3 + -3 + -3 = -9
+        System.out.println( possible + " " + Arrays.toString(list_nums) + " = " + target );
+
+    }
+    private static boolean canMakeSum2(int[] list_nums, int target) {
+
+        for (int num1 : list_nums) {
+            for (int num2 : list_nums) {
+                for (int num3 : list_nums) {
+                    if (num1 + num2 + num3 == target) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,5 +1,64 @@
-public class Exercise2 {
-    public static void Run() {
+import java.util.ArrayList;
 
+public class Exercise2 {
+    private static final int TOWER_RADIUS = 50;
+    private static final int TOWER_NUM    = 10;
+    private static final int MAP_XMAX     = 100;
+    private static final int MAP_YMAX     = 100;
+
+    private static ArrayList<Point> towerList = new ArrayList<>();
+    private static int[][] map;
+
+
+    public static void Run() {
+        map = new int[MAP_XMAX][MAP_YMAX];
+        addRandomTowers(50);
+        boolean covered = isCovered( towerList, 0, 0, 100, 100);
+        displayMap();
+        System.out.println( covered );
+    }
+
+    private static void addRandomTowers(int i) {
+        for (int tower = 0; tower < TOWER_NUM; tower++) {
+            Point p = new Point( randomInt(0, MAP_XMAX), randomInt(0, MAP_YMAX));
+            towerList.add( p );
+        }
+    }
+
+    private static int randomInt(int min, int max) {
+        return (int)(min + Math.random() * (max - min));
+    }
+
+    private static boolean isCovered(ArrayList<Point> towerList, int minX, int minY, int maxX, int maxY) {
+        boolean res = true;
+        for (int x = minX; x < maxX; x++) {
+            for (int y = minY; y < maxY; y++) {
+                Point p = new Point(x, y);
+
+                int coverage = 0;
+                for (Point tower : towerList) {
+                    if ( p.distance( tower ) < TOWER_RADIUS) {
+                        if (coverage < 10) {
+                            coverage++;
+                        }
+                        map[x][y] = coverage;
+                    }
+                }
+                if ( coverage == 0) {
+                    res = false;
+//                    return false;
+                }
+            }
+        }
+        return res;
+    }
+
+    private static void displayMap() {
+        for (int x = 0; x < map.length; x++) {
+            for (int y = 0; y < map[0].length; y++) {
+                System.out.print( map[x][y]);
+            }
+            System.out.println();
+        }
     }
 }
